e6e9f296ac5c51c05286fd77aaf688502431b508
SokeBiz <oladsodeeq148@gmail.com>
2023-05-17 09:06:54 +0100

    first deploy

---
+++ csc202.js
@@ -0,0 +1,33 @@
+var character = 'ogbon sodiki';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
+var age;
+age = 25;
+console.log(age);
+var details = [];
+details = ['wale', 'dele', 'wakili'];
+console.log(details);
+var info;
+info = { name: 'olu', age: 35, addy: true };
+console.log(info);
+var infos;
+infos = {
+    name: 'ogbeni',
+    age: 123,
+    addy: 'Wakajaye'
+};
+console.log(infos);
+var school;
+school = 'techU';
+school = 1;
+school = true;
+school = { tech: true, dept: 'computer' };
+school = ['cyber', 'computer', 'software'];
+console.log(school);
+var mixed = [];
+mixed.push('wale');
+console.log(mixed);
